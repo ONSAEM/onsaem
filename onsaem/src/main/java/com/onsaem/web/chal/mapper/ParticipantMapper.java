c9dcbfb0352e48dca191f3622fbcc992a6c21ade
뚀민 <@DESKTOP-I1U1PUT>
2023-01-06 00:59:06 +0900

    20230105 at home

--- onsaem/src/main/java/com/onsaem/web/chal/mapper/ParticipantMapper.java
+++ onsaem/src/main/java/com/onsaem/web/chal/mapper/ParticipantMapper.java
@@ -1,16 +1,27 @@
 package com.onsaem.web.chal.mapper;
 
+import java.util.List;
+
+import com.onsaem.web.chal.service.ParticipantVO;
+import com.onsaem.web.chal.service.PaymentVO;
+
 public interface ParticipantMapper {
+	
 	//챌린저스 참가
+	Integer inputParticipant(ParticipantVO vo);
 	
 	//챌린저스 참가 취소
-	
+	Integer delParticipant(String ParticipantId);
 	
 	//참가 시 결제
+	Integer inputPayment(PaymentVO vo);
 	
-	//참가 취소 시 환불
+	//참가 취소 시 결제테이블에서 삭제
+	Integer delPayment();
 	
-	//참가 인원 조회
+	//참가자 조회
+	List<ParticipantVO> listParticipantAll(String chalId);
 	
-	//참가 인원 세부 조회
+	//참가자 상세 조회
+	ParticipantVO getParticipant(ParticipantVO vo);
 }
