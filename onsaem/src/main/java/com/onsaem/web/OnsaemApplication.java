b7fec24df5dbbafb044b717611870bcd44ce9fc1
hye0n0 <hyeon97101@gmail.com>
2023-01-05 14:16:18 +0900

    commit

---
+++ onsaem/src/main/java/com/onsaem/web/OnsaemApplication.java
@@ -0,0 +1,13 @@
+package com.onsaem.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnsaemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnsaemApplication.class, args);
+	}
+
+}
