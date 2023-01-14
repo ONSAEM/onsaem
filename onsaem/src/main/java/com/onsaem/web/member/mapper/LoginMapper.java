c571dd8dec0debe903f6585a181de4b4675bbc0c
hye0n0 <112607811+hye0n0@users.noreply.github.com>
2023-01-14 18:20:16 +0900

    0114 SH DAY

--- onsaem/src/main/java/com/onsaem/web/member/mapper/LoginMapper.java
+++
@@ -1,9 +0,0 @@
-package com.onsaem.web.member.mapper;
-
-
-import com.onsaem.web.member.service.MemberVO;
-
-public interface LoginMapper {
-	// 로그인
-	MemberVO memberLogin(MemberVO vo);
-}
--- onsaem/src/main/java/com/onsaem/web/member/service/LoginService.java
+++
@@ -1,6 +0,0 @@
-package com.onsaem.web.member.service;
-
-public interface LoginService {
-	//로그인
-	MemberVO memberLogin(MemberVO vo);
-}
