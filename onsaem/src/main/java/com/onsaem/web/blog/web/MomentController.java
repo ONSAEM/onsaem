4a8e3c7a91c63acd7e9bf5facff7dfa5b95149e1
Kim-nemo <rhakdnj1232@naver.com>
2023-01-16 12:51:54 +0900

    0116 lunch

--- onsaem/src/main/java/com/onsaem/web/blog/web/MomentController.java
+++ onsaem/src/main/java/com/onsaem/web/blog/web/MomentController.java
@@ -1,5 +1,7 @@
 package com.onsaem.web.blog.web;
 
+import java.text.SimpleDateFormat;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -22,6 +24,8 @@ public class MomentController {
 	@ResponseBody
 	public MomentsVO getMoment(String momentId) {
 		MomentsVO vo = momentService.getMoment(momentId);
+
+		
 		return vo;
 	}
 }
