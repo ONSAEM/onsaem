a738576fe88f56171d82ceb075066f28c5365d9e
hye0n0 <hyeon97101@gmail.com>
2023-01-17 17:29:53 +0900

    0117 SH DAY

--- onsaem/src/main/java/com/onsaem/web/member/service/MemberService.java
+++ onsaem/src/main/java/com/onsaem/web/member/service/MemberService.java
@@ -1,5 +1,10 @@
 package com.onsaem.web.member.service;
 
+import java.util.List;
+
+import com.onsaem.web.common.service.MediaVO;
+
 public interface MemberService {
-	MemberVO getMember(String memberId);
+	public MemberVO getMember(String memberId);
+	public String sendAuthMail(String email,List<MediaVO> fileList);
 }
