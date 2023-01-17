f2bca4adf6c5e8f028b5bc2f6c276a45fa633ae9
Kim-nemo <rhakdnj1232@naver.com>
2023-01-17 17:29:18 +0900

    0117 집가서 클론하기..

--- onsaem/src/main/java/com/onsaem/web/blog/service/MomentService.java
+++ onsaem/src/main/java/com/onsaem/web/blog/service/MomentService.java
@@ -2,9 +2,15 @@ package com.onsaem.web.blog.service;
 
 import java.util.List;
 
+import com.onsaem.web.common.service.MediaVO;
+
 public interface MomentService {
 	// 한 블로거의 모먼트 전체 조회(4개만 출력)
 	public List<MomentsVO> getMomentList(MomentsVO momentsVO);
 	// 한 블로거의 모먼트 단건 조회
 	public MomentsVO getMoment(String momentId);
+	// 모먼트 등록
+	int momentInsert(MomentsVO momentsVO);
+	// 모먼트 사진 등록
+	int momentMediaInsert(MediaVO mediaVo);
 }
