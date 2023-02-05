48c0db9dd2482a49eee8baf8e2618dea57af87df
user <user@192.168.219.101>
2023-02-05 20:56:52 +0900

    20230205 8:56

--- onsaem/src/main/resources/static/shoptemplate/js/main.js
+++ onsaem/src/main/resources/static/shoptemplate/js/main.js
@@ -205,7 +205,7 @@
 	var proQty = $('.pro-qty');
 	proQty.prepend('<span class="dec qtybtn">-</span>');
 	proQty.append('<span class="inc qtybtn">+</span>');
-	proQty.on('click', '.qtybtn', functiondata,  data2) {
+	proQty.on('click', '.qtybtn', function() {
 		var $button = $(this);
 		var oldValue = $button.parent().find('input').val();
 		if ($button.hasClass('inc')) {
