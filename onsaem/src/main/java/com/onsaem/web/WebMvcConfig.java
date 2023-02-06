f285e56bd083ae5c3533e0e8d50a445ca37cb104
hye0n0 <hyeon97101@gmail.com>
2023-02-06 11:59:54 +0900

    commit

--- onsaem/src/main/java/com/onsaem/web/WebMvcConfig.java
+++ onsaem/src/main/java/com/onsaem/web/WebMvcConfig.java
@@ -9,6 +9,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/fileView/**")
-              .addResourceLocations("file:////home/onsaem_media/");
+              .addResourceLocations("file:////home/ubuntu/onsaem_media/");
     }
 }
